a491373a2580209f583a385e7a457260bc8b5ddc
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-02 12:31:14 -0400

    start

---
+++ snake_tutorial.py
@@ -0,0 +1,61 @@
+# snake tutorial in python from you-tube copied by Brian L
+
+import math
+import random
+import pygame
+import tkinter as tk 
+from tkinter import messagebox
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+def drawGrid(w, rows, surface):
+    pass
+
+
+def redrawWindow(surface):
+    pass
+
+def randomSnack(rows, items):
+    pass
+
+def message_box(subject, content):
+    pass
+
+def main():
+    pass
+
+
+rows = 
+w = 
+h =
+
+cube.rows = rows
+cube.w = w 
+    
+main()
